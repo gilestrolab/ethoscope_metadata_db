de86964a5cc4641d29189678abec3cbde8f7b836
Giorgio Gilestro <giorgio@gilest.ro>
2023-01-06 17:29:31 +0000

    First commit of a functional project

---
+++ metadata_db_server.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  metadata_db_server.py
+#  
+#  Copyright 2022 Giorgio Gilestro <giorgio@gilest.ro>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import os
+import logging
+import optparse
+import json
+import bottle
+
+from ast import literal_eval
+from ethoscopy.metadata_db import db_organiser, metadata_handler, metadata_crawler
+
+app = bottle.Bottle()
+STATIC_DIR = "./static"
+
+@app.route('/static/<filepath:path>')
+def server_static(filepath):
+    return bottle.static_file(filepath, root=STATIC_DIR)
+
+@app.route('/')
+def index():
+    return bottle.static_file('index.html', root=STATIC_DIR)
+
+@app.route('/save', method='POST')
+def save():
+
+    # text parameters that were received
+    #for k in bottle.request.forms.keys(): print (k, bottle.request.forms.get(k)) 
+
+    #file upload
+    allowed_extensions = ['.csv', '.tsv']
+
+    upload = bottle.request.files.get('file')
+    if not upload:
+        return {'success' : False, 'message' : 'A metadata file was not specified.'}
+
+    name, ext = os.path.splitext(upload.filename)
+    if ext not in allowed_extensions:
+        return {'success' : False, 'message' : 'Extensions allowed are %s.' % " / ".join(allowed_extensions)}
+
+    try:
+        upload.save(meta_db.upload_folder, overwrite=True)
+
+    except Exception as e:
+        print (e)
+        return {'success' : False, 'message' : str(e)}
+
+    try:
+        #After saving, creates the info file
+        uploaded_metadata = metadata_handler (  filename = os.path.join (meta_db.upload_folder, upload.filename), 
+                                                project = bottle.request.forms.get('project'),
+                                                tags = literal_eval(bottle.request.forms.get('tags')),
+                                                authors = literal_eval(bottle.request.forms.get('authors')),
+                                                doi = bottle.request.forms.get('doi'),
+                                                description = bottle.request.forms.get('description')
+                                                )
+        uploaded_metadata.save()
+
+        meta_db.refresh_all_info()
+
+        return {'success' : True, 'message' : 'Metadata file successfully uploaded.' }
+
+    except Exception as e:
+        return {'success' : False, 'message' : str(e)}
+
+
+@app.route('/download', method='GET')
+def download():
+    hash_id = bottle.request.query.get('id', '')
+    dwn_type = bottle.request.query.get('type', '')
+    fullpath = meta_db.request(hash_id)['filename']
+    path, filename = os.path.split( fullpath )
+    
+    if dwn_type == 'metadata':
+        return bottle.static_file (filename, root=path,  download=filename)
+
+    elif dwn_type == 'db_found':
+        md = metadata_handler(fullpath)
+        fn = os.path.splitext(filename)[0] + '.db_found.csv'
+        md.list_dbs(notfound=False).to_csv(os.path.join ('/tmp/', fn))
+        return bottle.static_file (fn, root='/tmp/',  download=fn)
+
+    elif dwn_type == 'db_notfound':
+        md = metadata_handler(fullpath)
+        fn = os.path.splitext(filename)[0] + '.db_notfound.csv'
+        md.list_dbs(notfound=True).to_csv(os.path.join ('/tmp/', fn))
+        return bottle.static_file (fn, root='/tmp/',  download=fn)
+        
+
+@app.route('/refresh', method='GET')
+def refresh():
+    hash_id = bottle.request.query.get('id', '')
+    filename = meta_db.request(hash_id)['filename']
+    md = metadata_handler ( filename )
+    md.associate_to_db(etho_db)
+    md.save()
+    return {'success' : True, 'message' : 'Metadata succefully associated.' }
+
+
+@app.route('/info_tree', method='GET')
+def get_metadata_tree():
+    return meta_db.all_projects
+
+
+@app.route('/metadata', method='GET')
+def get_metadata():
+    hash_id = bottle.request.query.get('id', '')
+    return meta_db.request(hash_id)
+
+@app.route('/known')
+def available_options():
+    return meta_db.available_options
+
+def close(exit_status=0):
+    logging.info("Closing server")
+    os._exit(exit_status)
+    
+    
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    parser = optparse.OptionParser()
+    parser.add_option("-d", "--db_path", dest="db_path", default="/mnt/ethoscope_results", help="Path to the root of the ethoscope db files")
+    parser.add_option("-f", "--db_file", dest="db_file", default="/opt/ethoscope_db", help="Path to the saved db file")
+
+    parser.add_option("-D", "--debug", dest="debug", default=False, help="Set DEBUG mode ON", action="store_true")
+    parser.add_option("-p", "--port", dest="port", default=8081, help="port")
+
+    (options, args) = parser.parse_args()
+
+    option_dict = vars(options)
+    DB_PATH = option_dict["db_path"]
+    DB_FILE = option_dict["db_file"]
+
+    PORT = option_dict["port"]
+    DEBUG = option_dict["debug"]
+
+    if DEBUG:
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.info("Logging using DEBUG SETTINGS")
+
+
+    etho_db = db_organiser(DB_PATH, refresh=False)
+    meta_db = metadata_crawler()
+
+    try:
+        app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
+
+    except KeyboardInterrupt:
+        logging.info("Stopping server cleanly")
+        pass
+
+    except socket.error as e:
+        logging.error(traceback.format_exc())
+        logging.error("Port %i is probably not accessible for you. Maybe use another one e.g.`-p 8000`" % PORT)
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
---
+++ static/js/script.js
@@ -0,0 +1,285 @@
+// materialize css tab menu
+// A Pen created on CodePen.io. Original URL: [https://codepen.io/frippa/pen/amBzrv](https://codepen.io/frippa/pen/amBzrv).
+// material fixed tab menu with materialize css
+
+$(".button-collapse").sideNav();
+
+//currently added authors and tags
+/* var current = {
+        authors_list : [{ tag: 'Giorgio Gilestro', }, { tag: 'Hannah Jones', }],
+        tags_list : [{ tag: 'uno', }, { tag: 'due', }]
+    }; */
+
+var current = {
+    authors_list : [],
+    tags_list : []
+}
+
+var info_tree = {};
+
+var formData = new FormData();
+var xhr = new XMLHttpRequest();
+
+
+"use strict";
+
+// Only start once the DOM tree is ready
+if(document.readyState === "complete") {
+    createDropzoneMethods();
+} else {
+    document.addEventListener("DOMContentLoaded", createDropzoneMethods);
+}
+
+
+function populate_select_with_projects() {
+    $.get( '/info_tree' ).success( function( data ) {
+        $.each(data, function (key, entry) {
+            $('#project-select').append($('<option></option>').attr('value', key).text(key));
+          })
+        $("#project-select").material_select();
+    })
+}
+
+function update_browser(identifier) {
+    $.get( '/metadata', {'id' : identifier} ).success( function( data ){
+        console.log(data);
+        $('#browser-table').show();
+
+        $('#browser_project').html (data['project']);
+        $('#browser_authors').html ( data['authors'].toString() );
+        $('#browser_tags').html ( data['tags'].toString() );
+        $('#browser_description').html (data['description'].toString());
+        
+        if (data['paper_doi']) {
+            $('#browser_doi').attr("href", data['paper_doi'] );
+            $('#browser_doi').show();
+        } else {
+            $('#browser_doi').hide();
+        }
+
+        $('#browser_download_link').attr("href", "/download?type=metadata&id="+data['identifier'] );
+        $('#browser_refresh').attr("onclick", "refresh_metadata_db('"+data['identifier']+"');" );
+
+        $('#browser_db_total').html (
+            data['db_files'] + ' databases <a href="/download?type=db_found&id=' +data['identifier']+ '" class="fa fa-download" style="color:grey" ></a>');
+
+        $('#browser_db_not_found').html (
+            data['db_files_na'] + ' databases <a href="/download?type=db_notfound&id=' +data['identifier']+ '" class="fa fa-download" style="color:grey"></a>');
+
+        $('#browser_db_size').html (data['db_files_size']);
+        $('#browser_entries_total').html (data['entries'] + ' entries');
+        $('#browser_entries_not_found').html (data['entries_not_found'] + ' entries');
+
+    })
+}
+
+
+function refresh_metadata_db(identifier) {
+    $('#refresh_modal').show();
+    $('#refresh_modal').focus();
+    $('#modal_text').html('Associating metadata to DB files. This may take a few seconds.');
+    $('#modal_btn').addClass('disabled');   
+    $('#modal_progress_bar').show();
+    
+    $.ajax({
+            url: '/refresh',
+            timeout: 60 * 1000,
+            data : {'id' : identifier},
+            type: 'GET'
+        }).success( function( data ){
+            $('#modal_progress_bar').hide();
+            $('#modal_text').html(data.message);
+            $('#modal_btn').removeClass('disabled');
+
+            $('#modal_btn').click(function(){ 
+                $('#refresh_modal').hide();
+            });
+
+
+    })
+}
+
+function refresh_info () {
+    $.get( '/info_tree' ).success( function( data ) {
+        $('#project-list').html('');
+        let prj_name = $("#project-select").val();
+        $.each(data[prj_name], function (filename, identifier) {
+            $('#project-list').append($('<a href="#" class="collection-item" onclick="update_browser(\''+identifier+'\');return false;"></a>').text(filename));
+          })
+    })
+}
+
+// INITIALIZATION OF AUTOCOMPLETE LIST
+function get_available_options () {
+    $.get( '/known').success( function( data ){
+        let available = {};
+
+        available.authors = Object.fromEntries(data['authors'].map(x => [x, null]));
+        available.project = Object.fromEntries(data['project'].map(x => [x, null]));
+        available.tags = Object.fromEntries(data['tags'].map(x => [x, null]));
+        available.paper_doi = Object.fromEntries(data['paper_doi'].map(x => [x, null]));
+        
+        $("#project-input").autocomplete({
+            data: available.project
+        });
+
+        $("#doi-input").autocomplete({
+            data: available.tags
+        });
+
+        $("#authors-input").autocomplete({
+            data: available.authors
+        });
+
+        $("#tags-input").autocomplete({
+            data: available.tags
+        });
+    });
+};
+
+// Display Chips
+function displayChips(divID) {
+
+    $('#' + divID).material_chip({
+        data: current[divID]
+    });
+}    
+
+function redirectFocus( from, dest) {    
+    $(from).focusin(function () {
+        $(dest).focus();
+    });
+}
+
+// ADDING A NEW CHIP
+function addChip ( divID ){
+    
+    input = divID + "-input";
+    list = divID + "_list";
+    
+    chipName = $("#" + input).val().toLowerCase();
+    
+    // test1 : minimum word size
+    if (!(chipName.length > 2)){
+        return 0;
+    }
+    // test2 :  no duplicates
+    for(i=0;i<current[list].length;i++) {
+        if(chipName == current[list][i].tag){
+            return 0;
+        }
+    }
+    // tests Okay => add the chip and refresh the view
+    current[list].push( {"tag" : chipName });
+   
+    displayChips(list);
+    $("#" + input).val("");
+    
+    return 1;
+};
+
+function createDropzoneMethods() {
+    let dropzone = document.getElementById("dropzone_element");
+
+    dropzone.ondragover = function() {
+        this.className = "dropzone dragover";
+        return false;
+    }
+    
+    dropzone.ondragleave = function() {
+        this.className = "dropzone";
+        return false;
+    }
+
+    dropzone.ondrop = function(e) {
+        // Stop browser from simply opening that was just dropped
+        e.preventDefault();  
+        
+        // Restore original dropzone appearance
+        //this.className = "dropzone";
+
+        dropzone.innerHTML = e.dataTransfer.files[0].name;
+
+        add_file_to_form(e.dataTransfer.files)
+    }    
+}
+
+function add_file_to_form(files) {
+    for(let i=0; i<files.length; i++) {
+        formData.append('file', files[i]);
+    }
+}
+
+
+function submit_form() {
+
+    formData.append ('project', $("#project-input").val());
+    formData.append ('doi', $("#doi-input").val());
+    formData.append ('description', $("#description-input").val());
+    formData.append ('authors', JSON.stringify(Object.entries(current.authors_list).map(([key, value]) => ( value['tag']))) );
+    formData.append ('tags', JSON.stringify(Object.entries(current.tags_list).map(([key, value]) => ( value['tag']))) );
+
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === XMLHttpRequest.DONE) {
+
+            let result = JSON.parse(xhr.response);
+
+            $("#upload_result_element").attr("hidden", false);
+
+            if (result.success == false) {
+                //make alert
+                $("#upload_result_element").attr('class', 'alert card red lighten-4 red-text text-darken-4" id="upload_result_element');
+                //$("#upload-banner-icon").innerHTML = "report";
+                $("#upload_result_message").html("<i class=\"material-icons\" id=\"upload-banner-icon\">report</i><span>Error:</span>" + result.message);
+            }
+
+            if (result.success == true) {
+                //make green
+                $("#upload_result_element").attr('class', 'alert card green lighten-4 green-text text-darken-4" id="upload_result_element');
+                //$("#upload-banner-icon").innerHTML = "check_circle";
+                $("#upload_result_message").html("<i class=\"material-icons\" id=\"upload-banner-icon\">check_circle</i><span>OK:</span>" + result.message);
+            }
+        }
+    }
+
+    xhr.open('POST', '/save', true); // async = true
+    xhr.send(formData); 
+}
+
+$(function() {
+// delete chip command
+    $('#current_authors_list').on('chip.delete', function(e, chip){
+        current_authors = $("#current_authors_list").material_chip('data');
+    });
+
+    $('.modal').modal();
+
+    get_available_options();
+
+    redirectFocus('#authors_list', '#authors-input');
+    redirectFocus('#tags_list', '#tags-input');
+
+    displayChips('authors_list');
+    displayChips('tags_list');
+
+    $("#add_authors_BTN").click(function () {
+        addChip( 'authors' );
+    });
+
+    $("#add_tags_BTN").click(function () {
+        addChip( 'tags' );
+    });
+
+    $("#save_BTN").click(function () {
+        submit_form();
+    });
+
+    $('#project-select').material_select();
+    populate_select_with_projects();
+
+    $('#project-select').on('change', function() {
+        refresh_info();
+    });
+
+});
